f38793e77a2ffa504493d696c2fd0d387926e47c
Tyler Miess <tmiess19@gmail.com>
2018-01-20 07:22:19 +0000

    added new logo for mobile and tablet responsiveness

--- public/app.js
+++ public/app.js
@@ -97,3 +97,28 @@ $('body').on("click", ".close", function() {
     console.log("modal-close button works");
     $(".modal").removeClass("is-active");
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("burger js connected");
+    // Get all "navbar-burger" elements
+    var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function($el) {
+            $el.addEventListener('click', function() {
+
+                // Get the target from the "data-target" attribute
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+
+                // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+});
